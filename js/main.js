55957c6bd27f7faf9f44d58ab8362cbe3d404a2e
Marcos Corrêa <marcowcorrea01@gmail.com>
2023-08-30 17:03:25 -0300

    retirada dos emails e adicao das animações 😏

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('header', {reset: true, delay: 300, origin: "top", distance: '60px'});
+ScrollReveal().reveal('.body', {reset: true, delay: 400, origin: "bottom", distance: '60px'});
+ScrollReveal().reveal('.b3', {reset: true, delay: 300, origin: "right", distance: '60px'});
+ScrollReveal().reveal('.b4', {reset: true, delay: 400, origin: "right", distance: '60px'});
+ScrollReveal().reveal('.b5', {reset: true, delay: 500, origin: "right", distance: '60px'});
+ScrollReveal().reveal('.b6', {reset: true, delay: 600, origin: "right", distance: '60px'});
+ScrollReveal().reveal('.c3', {reset: true, delay: 300, origin: "bottom", distance: '60px'});
+ScrollReveal().reveal('.c4', {reset: true, delay: 400, origin: "bottom", distance: '60px'});
+ScrollReveal().reveal('.c5', {reset: true, delay: 500, origin: "bottom", distance: '60px'});
+ScrollReveal().reveal('.l3', {reset: true, delay: 300, origin: "left", distance: '60px'});
+ScrollReveal().reveal('.r3', {reset: true, delay: 300, origin: "right", distance: '60px'});
+ScrollReveal().reveal('.t6', {reset: true, delay: 600, origin: "top", distance: '60px'});
+ScrollReveal().reveal('.t3', {reset: true, delay: 300, origin: "top", distance: '60px'});
