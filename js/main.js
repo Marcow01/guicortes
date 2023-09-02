1aa1f66daf440be9478ad8612efcc2788a8c5b55
Marcos Corrêa <marcowcorrea01@gmail.com>
2023-09-02 16:14:04 -0300

    resolução de problemas

--- js/main.js
+++ js/main.js
@@ -1,129 +1,77 @@
 ScrollReveal().reveal('header', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 300,
   origin: 'top',
   distance: '60px',
 });
 ScrollReveal().reveal('.body', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 400,
   origin: 'bottom',
   distance: '60px',
 });
 ScrollReveal().reveal('.b3', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 300,
   origin: 'right',
   distance: '60px',
 });
 ScrollReveal().reveal('.b4', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 400,
   origin: 'right',
   distance: '60px',
 });
 ScrollReveal().reveal('.b5', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 500,
   origin: 'right',
   distance: '60px',
 });
 ScrollReveal().reveal('.b6', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 600,
   origin: 'right',
   distance: '60px',
 });
 ScrollReveal().reveal('.c3', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 300,
   origin: 'bottom',
   distance: '60px',
 });
 ScrollReveal().reveal('.c4', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 400,
   origin: 'bottom',
   distance: '60px',
 });
 ScrollReveal().reveal('.c5', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 500,
   origin: 'bottom',
   distance: '60px',
 });
 ScrollReveal().reveal('.l3', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 300,
   origin: 'left',
   distance: '60px',
 });
 ScrollReveal().reveal('.r3', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 300,
   origin: 'right',
   distance: '60px',
 });
 ScrollReveal().reveal('.t6', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 600,
   origin: 'top',
   distance: '60px',
 });
 ScrollReveal().reveal('.t3', {
-<<<<<<< HEAD
   reset: false,
-=======
-  reset: true,
->>>>>>> 55c9e70d57ec9a5cdc041a3c508d5dea55bbe0f4
   delay: 300,
   origin: 'top',
   distance: '60px',
