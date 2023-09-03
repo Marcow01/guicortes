a6daa34aa5c0babc9c242401a9ab6be05136e302
Marcos Corrêa <marcowcorrea01@gmail.com>
2023-09-03 04:39:25 -0300

    deslizamento da barra lateral

---
+++ js/send.php
@@ -0,0 +1,33 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'guicortes5500@gmail.com'; 
+    $mail->Password = 'bqithtvlgucyktgw';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom($_POST["email"], $_POST["nome"], $_POST["tel"]);
+    $mail->addAddress('estrellachann@gmail.com');
+    $mail->isHTML(true);
+    $mail->Subject = 'MENSAGEM';
+    $mail->Body = "nome: {$_POST['nome']}<br>email: {$_POST['email']}<br>mensagem: {$_POST['mensagem']}<br>telefone: {$_POST["tel"]}";
+
+    $mail->send();
+    header('Location:../index.html');
+    exit();
+} catch (Exception $e) {
+    echo "erro ao enviar o email: " . $mail->ErrorInfo;
+}
+
+?>
