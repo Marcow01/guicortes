46368afa771fdc9336247345a2ea05a9c101a0b2
gabrel063 <cuecacuzcuz@gmail.com>
2023-09-05 18:58:51 -0300

    alteração no readme

---
+++ README.md
@@ -0,0 +1,3 @@
+<h3>🥰 &nbsp; gui cortes </h3>
+
+🌱 &nbsp; repositório de desenvolvimento do site gui cortes
--- js/main.js
+++ js/main.js
@@ -1,13 +1,143 @@
-ScrollReveal().reveal('header', {reset: true, delay: 300, origin: "top", distance: '60px'});
-ScrollReveal().reveal('.body', {reset: true, delay: 400, origin: "bottom", distance: '60px'});
-ScrollReveal().reveal('.b3', {reset: true, delay: 300, origin: "right", distance: '60px'});
-ScrollReveal().reveal('.b4', {reset: true, delay: 400, origin: "right", distance: '60px'});
-ScrollReveal().reveal('.b5', {reset: true, delay: 500, origin: "right", distance: '60px'});
-ScrollReveal().reveal('.b6', {reset: true, delay: 600, origin: "right", distance: '60px'});
-ScrollReveal().reveal('.c3', {reset: true, delay: 300, origin: "bottom", distance: '60px'});
-ScrollReveal().reveal('.c4', {reset: true, delay: 400, origin: "bottom", distance: '60px'});
-ScrollReveal().reveal('.c5', {reset: true, delay: 500, origin: "bottom", distance: '60px'});
-ScrollReveal().reveal('.l3', {reset: true, delay: 300, origin: "left", distance: '60px'});
-ScrollReveal().reveal('.r3', {reset: true, delay: 300, origin: "right", distance: '60px'});
-ScrollReveal().reveal('.t6', {reset: true, delay: 600, origin: "top", distance: '60px'});
-ScrollReveal().reveal('.t3', {reset: true, delay: 300, origin: "top", distance: '60px'});
+ScrollReveal().reveal('header', {
+    reset: false,
+    delay: 300,
+    origin: 'top',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.c3', {
+    reset: false,
+    delay: 300,
+    origin: 'bottom',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.c4', {
+    reset: false,
+    delay: 400,
+    origin: 'bottom',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.c5', {
+    reset: false,
+    delay: 500,
+    origin: 'bottom',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.c6', {
+    reset: false,
+    delay: 600,
+    origin: 'bottom',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.l3', {
+    reset: false,
+    delay: 300,
+    origin: 'left',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.r3', {
+    reset: false,
+    delay: 300,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.t6', {
+    reset: false,
+    delay: 600,
+    origin: 'top',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.t3', {
+    reset: false,
+    delay: 300,
+    origin: 'top',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.t5', {
+    reset: false,
+    delay: 300,
+    origin: 'top',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-300', {
+    reset: false,
+    delay: 300,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-350', {
+    reset: false,
+    delay: 350,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-400', {
+    reset: false,
+    delay: 400,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-450', {
+    reset: false,
+    delay: 450,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-500', {
+    reset: false,
+    delay: 500,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-550', {
+    reset: false,
+    delay: 550,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-600', {
+    reset: false,
+    delay: 600,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.direita-650', {
+    reset: false,
+    delay: 650,
+    origin: 'right',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.abaixo-300', {
+    reset: false,
+    delay: 300,
+    origin: 'bottom',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.abaixo-400', {
+    reset: false,
+    delay: 400,
+    origin: 'bottom',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.abaixo-500', {
+    reset: false,
+    delay: 500,
+    origin: 'bottom',
+    distance: '5px',
+  });
+  ScrollReveal().reveal('.topo-550', {
+    reset: false,
+    delay: 550,
+    origin: 'top',
+    distance: '5px',
+  });
+
+  var typed = new Typed('#typedtext', {
+    strings: ['negócio.', 'trabalho.', 'projeto.'],
+    typeSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    backDelay: 2000,
+    backSpeed: 100,
+    showCursor: true,
+  });
+  
